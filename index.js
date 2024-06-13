0d1c4d3f516c4131d3b7d459e573c25200baba73
Svhodro <rudrolipi@gmail.com>
2024-06-13 06:43:43 +0600

    mongodb url change

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(
     cors({
       origin: [
         "http://localhost:5173",
-        "https://amazon-clone-sporsho.netlify.app",
+        "https://amazon-work-clone.netlify.app",
         // "https://cardoctor-bd.firebaseapp.com",
       ],
     })
